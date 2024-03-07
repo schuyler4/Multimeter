58e061a911306feeb17fb8ae17f75263f3889dc8
Marek Newton <marek.s.newton@gmail.com>
2024-03-07 14:59:26 -0700

    python cap calc

--- cap_charge_interpolation/real_real.py
+++ cap_charge_interpolation/real_real.py
@@ -1,10 +1,12 @@
-'''
-0.869472
+'''0.869472, 1.145098,'''
+from capacitance_finder import Capacitance_Finder
 
-1.145098
+voltage_points = [0.87, 1.14]
+series_resistor = 10000
+voltage_source = 2.49/2
+tt = 0.00177
 
-1.280971
+cap_finder = Capacitance_Finder(voltage_points, series_resistor, voltage_source, tt)
+print(cap_finder())
 
-1.306792
-'''
 
